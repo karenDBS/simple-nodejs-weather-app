70fc117ea841aa9990783db2a4950d1d8b3336f9
Abril-Perez <pereznovasabril@gmail.com>
2019-09-18 16:38:43 -0300

    dsd

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const app = express()
+const app = express();
 
 const apiKey = '*****************';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   res.render('index', {weather: null, error: null});
